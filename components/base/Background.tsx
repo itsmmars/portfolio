6ace8ddb5e661c3dcdcf32903139770a6f14d6fb
itsmmars <mmars.dev@gmail.com>
2023-02-16 21:57:57 -0800

    initial three fiber background built out

---
+++ components/base/Background.tsx
@@ -0,0 +1,46 @@
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
+import { inSphere } from 'maath/random'
+import { Points, PointMaterial } from '@react-three/drei'
+
+function Stars(props: any) {
+  const ref = useRef<THREE.Mesh>(null!)
+  const [sphere] = useState(() => inSphere(new Float32Array(5000), { radius: 5 }))
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10
+    ref.current.rotation.y -= delta / 15
+  })
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points 
+        {...props}
+        ref={ref} 
+        positions={sphere} 
+        stride={3} 
+        frustumCulled={false}> 
+        <PointMaterial 
+          transparent color="red" 
+          size={0.025} 
+          sizeAttenuation={true} 
+          depthWrite={false} />
+      </Points>
+    </group>
+  )
+}
+
+export const Background = () => {
+  return (
+    <Canvas style={{ 
+      position: 'fixed', 
+      top: 0, 
+      left: 0, 
+      zIndex: -1, 
+      backgroundColor: 'white', 
+      filter: 'blur(3px)',
+      width: '100%', 
+      height: '100%'
+    }}>
+      <Stars />
+    </Canvas>
+  )
+}
