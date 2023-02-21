31d025aabeee347e867fabf981ab6f68dbeede4b
itsmmars <mmars.dev@gmail.com>
2023-02-21 11:01:47 -0800

    refactored portfolio component. MFCard component, types, and ProjProps type created and implemented.

---
+++ components/base/types.ts
@@ -0,0 +1,11 @@
+export interface ProjProps {
+  name: string
+  logo?: string
+  filter: boolean
+  alt: string
+  url: string
+  imgUrl: string
+  summary: string
+  overview: string
+  tech: string[]
+}
