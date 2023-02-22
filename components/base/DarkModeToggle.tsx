982c6e8885fc8b7e0d10a0d727c1b3022741f1ac
itsmmars <mmars.dev@gmail.com>
2023-02-21 19:44:06 -0800

    ColorMode implemented on mobile

--- components/base/DarkModeToggle.tsx
+++ components/base/DarkModeToggle.tsx
@@ -1,16 +1,26 @@
-import { useColorMode, IconButton } from '@chakra-ui/react'
+import { useColorMode, IconButton, ResponsiveValue } from '@chakra-ui/react'
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 
-export const DarkModeToggle = () => {
+interface ToggleProps {
+  position: any,
+  top: number,
+  right: number
+}
+
+export const DarkModeToggle = ({
+  position,
+  top,
+  right
+}: ToggleProps) => {
   const { colorMode, toggleColorMode } = useColorMode()
   const isDark = colorMode === 'dark'
   return (
     <IconButton
-      position="fixed"
-      top={4}
-      right={4}
+      position={position}
+      top={top}
+      right={right}
       icon={isDark ? <SunIcon /> : <MoonIcon />}
-      aria-label="Toggle Theme"
+      aria-label='Toggle Theme'
       variant='custom'
       onClick={toggleColorMode}
     />
